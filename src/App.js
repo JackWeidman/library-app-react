647a0f3fc39c73e8a6d089ee09e10ca0076388e2
Jack Weidman <jackweidman1@gmail.com>
2024-06-04 21:11:52 -0500

    second react commit, just learning the ropes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:</label>
+      <input type="text"></input>
+    </form>
+  );
+}
+
+const products = [
+  { label: 'Item 1', id: 1 },
+  { label: 'Item 2', id: 2 },
+  { label: 'Item 3', id: 3 },
+];
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <label>Enter your name:</label>
-          <input type='text'></input>
-        </form>
+        <h1>
+          Hello there.
+          <br />
+          How do you do?
+        </h1>
+        <MyForm />
       </header>
+      <div className="App-header">
+        <h1>This is a new div</h1>
+      </div>
     </div>
   );
 }
